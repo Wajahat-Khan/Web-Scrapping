2a7b5ea54b18994e14bd0012249140e33c89cbe0
Wajahat Hussain <wajahat.hussain@northbaysolutions.net>
2019-12-08 18:56:08 +0500

    navigation done

--- navigating-trees.py
+++ navigating-trees.py
@@ -4,3 +4,6 @@ html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html, 'html.parser')
 for sibling in bs.find('table', {'id':'giftList'}).tr.descendants:
     print(sibling)
+
+
+# .parents, .parents, .next_sibling(s), .previous_sibling(s) are also there
